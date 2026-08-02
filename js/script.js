caa4f94144135a5a49750a47042ef0b78048233c
Arielson-art <reisarielson36@gmail.com>
2026-08-01 22:07:33 -0300

    Add dark mode toggle functionality

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ const botao = document.querySelector("button");
 botao.addEventListener("click", function(){
     window.location.href = "materias.html";
 });
+const botaoModo = document.getElementById("modoEscuro");
+
+botaoModo.addEventListener("click", function(){
+
+    document.body.classList.toggle("modo-escuro");
+
+});
